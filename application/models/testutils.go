7157bd26d531dba0b73646c81e55a7daaa58ade7
Schparky <3172830+Schparky@users.noreply.github.com>
2021-08-03 16:46:37 -0400

    create user fixtures

--- application/models/testutils.go
+++ application/models/testutils.go
@@ -1,16 +1,18 @@
 // +build development
 
 // This build tag ensures that this file will not be included unless
-//  the `testing` tag is explicitly requested (which should be never)
+//  the `development` tag is explicitly requested (which should be never)
 
 package models
 
 import (
 	"fmt"
-	"reflect"
+	"math/rand"
 	"strconv"
+	"time"
 
 	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/nulls"
 	"github.com/gobuffalo/pop/v5"
 
 	"github.com/silinternational/riskman-api/domain"
@@ -41,42 +43,46 @@ func CreateTestContext(user User) buffalo.Context {
 	return ctx
 }
 
-// MustCreate saves a record to the database. Panics if any error occurs.
-func MustCreate(tx *pop.Connection, f interface{}) {
-	value := reflect.ValueOf(f)
-
-	if value.Type().Kind() != reflect.Ptr {
-		panic("MustCreate requires a pointer")
-	}
-
-	uuidField := value.Elem().FieldByName("UUID")
-	if uuidField.IsValid() {
-		uuidField.Set(reflect.ValueOf(domain.GetUUID()))
-	}
-
-	err := tx.Create(f)
-	if err != nil {
-		panic(fmt.Sprintf("error creating %T fixture, %s", f, err))
-	}
-}
-
 // UserFixtures hold slices of model objects created with new user fixtures
 type UserFixtures struct {
 	Users
+	UserAccessTokens
 }
 
-// CreateUserFixtures generates any number of user records for testing.
+// CreateUserFixtures generates any number of user records for testing. The access token for
+// each user is the same as the user's Email.
 func CreateUserFixtures(tx *pop.Connection, n int) UserFixtures {
-
 	unique := domain.GetUUID().String()
 
 	users := make(Users, n)
+	accessTokenFixtures := make(UserAccessTokens, n)
 	for i := range users {
-		users[i].Email = unique + "_user" + strconv.Itoa(i) + "@example.com"
+		users[i].Email = fmt.Sprintf("user%d_%s@example.com", i, unique)
+		iStr := strconv.Itoa(i)
+		users[i].FirstName = "first" + iStr
+		users[i].LastName = "last" + iStr
+		users[i].LastLoginUTC = time.Now()
+		users[i].StaffID = strconv.Itoa(rand.Int())
 		MustCreate(tx, &users[i])
+
+		accessTokenFixtures[i].UserID = users[i].ID
+		accessTokenFixtures[i].AccessToken = HashClientIdAccessToken(users[i].Email)
+		accessTokenFixtures[i].ExpiresAt = time.Now().Add(time.Minute * 60)
+		accessTokenFixtures[i].LastUsedAt = nulls.NewTime(time.Now())
+		MustCreate(tx, &accessTokenFixtures[i])
 	}
 
 	return UserFixtures{
-		Users: users,
+		Users:            users,
+		UserAccessTokens: accessTokenFixtures,
+	}
+}
+
+// MustCreate saves a record to the database with validation. Panics if any error occurs.
+func MustCreate(tx *pop.Connection, f interface{}) {
+	// Use `create` instead of `tx.Create` to check validation rules
+	err := create(tx, f)
+	if err != nil {
+		panic(fmt.Sprintf("error creating %T fixture, %s", f, err))
 	}
 }
