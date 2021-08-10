e846500f864ee95d7de4cd34bd312fc39695a4bc
Schparky <3172830+Schparky@users.noreply.github.com>
2021-08-10 16:19:25 -0600

    move doc generation utils into Dockerfile-api-docs

--- application/build-push-api-docs.sh
+++ application/build-push-api-docs.sh
@@ -8,14 +8,8 @@ set -x
 # generate swagger.json
 swagger generate spec -m -o /src/swagger.json
 
-# install redoc-cli to bundle html
-apk add --no-cache npm
-npm install -g redoc-cli
+# bundle doc html
 redoc-cli bundle --cdn --title "Riskman API Documentation" --output api-docs.html /src/swagger.json
 
-# install aws cli to push docs to s3
-apk add --no-cache python3 py3-pip
-pip install awscli
-
 # copy file to s3
-aws s3 cp api-docs.html $S3PATH
+#aws s3 cp api-docs.html $S3PATH
