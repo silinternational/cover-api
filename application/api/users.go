c9cb4bc8476146aa01e65630c3fc81dd637c3de2
Schparky <3172830+Schparky@users.noreply.github.com>
2021-08-10 16:37:09 -0600

    swagger docs for Users

--- application/api/users.go
+++ application/api/users.go
@@ -7,13 +7,31 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+// swagger:model
 type Users []User
 
+// app user
+// swagger:model
 type User struct {
-	ID           uuid.UUID  `json:"id"`
-	Email        string     `json:"email"`
-	FirstName    string     `json:"first_name"`
-	LastName     string     `json:"last_name"`
-	LastLoginUTC time.Time  `json:"last_login_utc"`
-	PolicyID     nulls.UUID `json:"policy_id"` // TODO: provide either a list of IDs or a list of Policies
+	// unique ID
+	//
+	// read only: true
+	// swagger:strfmt uuid4
+	ID uuid.UUID `json:"id"`
+
+	// email address
+	Email string `json:"email"`
+
+	// first name
+	FirstName string `json:"first_name"`
+
+	// last name
+	LastName string `json:"last_name"`
+
+	// last login date and time (UTC)
+	LastLoginUTC time.Time `json:"last_login_utc"`
+
+	// policy ID (temporary, will be replaced with a list of policies)
+	// swagger:strfmt uuid4
+	PolicyID nulls.UUID `json:"policy_id"` // TODO: provide either a list of IDs or a list of Policies
 }
