8e9739bdfaf0ee40f82cf657bba7c714db42dc36
Steve Bagwell <steve_bagwell@sil.org>
2021-08-18 15:53:06 -0400

    remove ses email stuff from storage package

--- application/storage/storage.go
+++ application/storage/storage.go
@@ -6,8 +6,6 @@ import (
 	// see commented out code at the end of function "rawEmail")
 	"errors"
 	"fmt"
-	"mime/multipart"
-	"net/textproto"
 	"net/url"
 	"time"
 
@@ -16,9 +14,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/ses"
-	"jaytaylor.com/html2text"
-
 	"github.com/silinternational/riskman-api/domain"
 )
 
@@ -184,162 +179,4 @@ func CreateS3Bucket() error {
 		}
 	}
 	return nil
-}
-
-// SendEmail sends a message using SES
-func SendEmail(to, from, subject, body string) error {
-	svc, err := createSESService(getSESConfigFromEnv())
-	if err != nil {
-		return fmt.Errorf("SendEmail failed creating SES service, %s", err)
-	}
-
-	input := &ses.SendRawEmailInput{
-		RawMessage: &ses.RawMessage{Data: rawEmail(to, from, subject, body)},
-		Source:     aws.String(from),
-	}
-
-	result, err := svc.SendRawEmail(input)
-	if err != nil {
-		return fmt.Errorf("SendEmail failed using SES, %s", err)
-	}
-
-	domain.Logger.Printf("Message sent using SES, message ID: %s", *result.MessageId)
-	return nil
-}
-
-// rawEmail generates a multi-part MIME email message with a plain text, html text, and inline logo attachment as
-// follows:
-//
-// * multipart/alternative
-//   * text/plain
-//   * multipart/related
-//     * text/html
-//     * image/png
-//
-// Abbreviated example of the generated email message:
-//  From: from@example.com
-//	To: to@example.com
-//	Subject: subject text
-//	Content-Type: multipart/alternative; boundary="boundary_alternative"
-//
-//	--boundary_alternative
-//	Content-Type: text/plain; charset=utf-8
-//
-//	Plain text body
-//	--boundary_alternative
-//	Content-type: multipart/related; boundary="boundary_related"
-//
-//	--boundary_related
-//	Content-Type: text/html; charset=utf-8
-//
-//	HTML body
-//	--boundary_related
-//	Content-Type: image/png
-//	Content-Transfer-Encoding: base64
-//	Content-ID: <logo>
-//	--boundary_related--
-//	--boundary_alternative--
-func rawEmail(to, from, subject, body string) []byte {
-	tbody, err := html2text.FromString(body)
-	if err != nil {
-		domain.Logger.Printf("error converting html email to plain text ... %s", err.Error())
-		tbody = body
-	}
-
-	b := &bytes.Buffer{}
-
-	b.WriteString("From: " + from + "\n")
-	b.WriteString("To: " + to + "\n")
-	b.WriteString("Subject: " + subject + "\n")
-	b.WriteString("MIME-Version: 1.0\n")
-
-	alternativeWriter := multipart.NewWriter(b)
-	b.WriteString(`Content-Type: multipart/alternative; type="text/plain"; boundary="` +
-		alternativeWriter.Boundary() + `"` + "\n\n")
-
-	w, err := alternativeWriter.CreatePart(textproto.MIMEHeader{
-		"Content-Type":        {"text/plain; charset=utf-8"},
-		"Content-Disposition": {"inline"},
-	})
-	if err != nil {
-		domain.ErrLogger.Printf("failed to create MIME text part, %s", err)
-	} else {
-		_, _ = fmt.Fprint(w, tbody)
-	}
-
-	relatedWriter := multipart.NewWriter(b)
-	_, err = alternativeWriter.CreatePart(textproto.MIMEHeader{
-		"Content-Type": {`multipart/related; type="text/html"; boundary="` + relatedWriter.Boundary() + `"`},
-	})
-	if err != nil {
-		domain.ErrLogger.Printf("failed to create MIME related part, %s", err)
-	}
-
-	w, err = relatedWriter.CreatePart(textproto.MIMEHeader{
-		"Content-Type":        {"text/html; charset=utf-8"},
-		"Content-Disposition": {"inline"},
-	})
-	if err != nil {
-		domain.ErrLogger.Printf("failed to create MIME html part, %s", err)
-	} else {
-		_, _ = fmt.Fprint(w, body)
-	}
-
-	// Uncomment the code below to add the ability to display a logo at
-	// the end of an email. This code needs "logo.png" located in
-	// "riskman-api/applications/assets" to function correctly.
-	// Also uncomment "encoding/base64" in imports section.
-
-	/*w, err = relatedWriter.CreatePart(textproto.MIMEHeader{
-		"Content-Type":              {"image/png"},
-		"Content-Disposition":       {"inline"},
-		"Content-ID":                {"<logo>"},
-		"Content-Transfer-Encoding": {"base64"},
-	})
-	if err != nil {
-		domain.ErrLogger.Printf("failed to create MIME image part, %s", err)
-	} else {
-		logo, err := domain.Assets.Find("logo.png")
-		if err != nil {
-			domain.ErrLogger.Printf("failed to read logo file, %s", err)
-		}
-		encoder := base64.NewEncoder(base64.StdEncoding, b)
-		_, err = encoder.Write(logo)
-		if err != nil {
-			domain.ErrLogger.Printf("failed to write logo to email, %s", err)
-		}
-		err = encoder.Close()
-		if err != nil {
-			domain.ErrLogger.Printf("failed to close logo base64 encoder, %s", err)
-		}
-	}
-
-	if err = relatedWriter.Close(); err != nil {
-		domain.ErrLogger.Printf("failed to close MIME related part, %s", err)
-	}
-
-	if err = alternativeWriter.Close(); err != nil {
-		domain.ErrLogger.Printf("failed to close MIME alternative part, %s", err)
-	}*/
-
-	return b.Bytes()
-}
-
-func getSESConfigFromEnv() awsConfig {
-	return awsConfig{
-		awsAccessKeyID:     domain.Env.AwsAccessKeyID,
-		awsSecretAccessKey: domain.Env.AwsSecretAccessKey,
-		awsRegion:          domain.Env.AwsRegion,
-	}
-}
-
-func createSESService(config awsConfig) (*ses.SES, error) {
-	sess, err := session.NewSession(&aws.Config{
-		Credentials: credentials.NewStaticCredentials(config.awsAccessKeyID, config.awsSecretAccessKey, ""),
-		Region:      aws.String(config.awsRegion),
-	})
-	if err != nil {
-		return nil, err
-	}
-	return ses.New(sess), nil
 }
