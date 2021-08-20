f8ee992d50630949714de46dcaef3bee6c438be3
Phillip Shipley <phillip.shipley@gmail.com>
2021-08-20 17:44:04 -0400

    ignore gosec warning about rand.Intn

--- application/domain/domain.go
+++ application/domain/domain.go
@@ -328,7 +328,7 @@ var letters = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 func RandomString(n int) string {
 	b := make([]rune, n)
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = letters[rand.Intn(len(letters))] // #nosec G404
 	}
 	return string(b)
 }
