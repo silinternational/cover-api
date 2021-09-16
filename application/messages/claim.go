069838d0237a2483f01e1a7ea4e7b67163aa51fc
Steve Bagwell <steve_bagwell@sil.org>
2021-09-16 11:32:40 -0400

    Add doc comments

--- application/messages/claim.go
+++ application/messages/claim.go
@@ -7,6 +7,8 @@ import (
 	"github.com/silinternational/cover-api/models"
 )
 
+// ClaimReview1QueueMessage queues messages to the stewards to
+//  notify them that a claim has been submitted for preapproval
 func ClaimReview1QueueMessage(tx *pop.Connection, claim models.Claim) {
 	claim.LoadPolicyMembers(models.DB, false)
 	memberName := claim.Policy.Members[0].Name()
@@ -33,6 +35,8 @@ func ClaimReview1QueueMessage(tx *pop.Connection, claim models.Claim) {
 	notn.CreateNotificationUsersForStewards(tx)
 }
 
+// ClaimRevisionQueueMessage queues messages to the claim's members to
+//  notify them that revisions are required on their claim
 func ClaimRevisionQueueMessage(tx *pop.Connection, claim models.Claim) {
 	claim.LoadPolicyMembers(models.DB, false)
 
@@ -61,6 +65,8 @@ func ClaimRevisionQueueMessage(tx *pop.Connection, claim models.Claim) {
 	}
 }
 
+// ClaimPreapprovedQueueMessage queues messages to the claim's members to
+//  notify them that their claim has been preapproved and requires receipts
 func ClaimPreapprovedQueueMessage(tx *pop.Connection, claim models.Claim) {
 	claim.LoadPolicyMembers(models.DB, false)
 
@@ -89,6 +95,8 @@ func ClaimPreapprovedQueueMessage(tx *pop.Connection, claim models.Claim) {
 	}
 }
 
+// ClaimReceiptQueueMessage queues messages to the claim's members to
+//  notify them that their claim requires receipts (again)
 func ClaimReceiptQueueMessage(tx *pop.Connection, claim models.Claim) {
 	claim.LoadPolicyMembers(models.DB, false)
 
@@ -117,6 +125,8 @@ func ClaimReceiptQueueMessage(tx *pop.Connection, claim models.Claim) {
 	}
 }
 
+// ClaimReview2QueueMessage queues messages to the stewards to
+//  notify them that a claim has been submitted to Review2 status
 func ClaimReview2QueueMessage(tx *pop.Connection, claim models.Claim) {
 	claim.LoadPolicyMembers(models.DB, false)
 	memberName := claim.Policy.Members[0].Name()
@@ -143,6 +153,8 @@ func ClaimReview2QueueMessage(tx *pop.Connection, claim models.Claim) {
 	notn.CreateNotificationUsersForStewards(tx)
 }
 
+// ClaimReview3QueueMessage queues messages to the signators to
+//  notify them that a claim has been submitted to Review3 status
 func ClaimReview3QueueMessage(tx *pop.Connection, claim models.Claim) {
 	claim.LoadPolicyMembers(models.DB, false)
 	memberName := claim.Policy.Members[0].Name()
@@ -169,6 +181,8 @@ func ClaimReview3QueueMessage(tx *pop.Connection, claim models.Claim) {
 	notn.CreateNotificationUsersForSignators(tx)
 }
 
+// ClaimApprovedQueueMessage queues messages to a claim's members to
+//  notify them that it has been approved
 func ClaimApprovedQueueMessage(tx *pop.Connection, claim models.Claim) {
 	claim.LoadPolicyMembers(models.DB, false)
 
@@ -196,6 +210,8 @@ func ClaimApprovedQueueMessage(tx *pop.Connection, claim models.Claim) {
 	}
 }
 
+// ClaimDeniedQueueMessage queues messages to a claim's members to
+//  notify them that it has been denied
 func ClaimDeniedQueueMessage(tx *pop.Connection, claim models.Claim) {
 	claim.LoadPolicyMembers(models.DB, false)
 
