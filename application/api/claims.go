fdb97992f5931051a02d2f871089260daa2d92e6
Schparky <3172830+Schparky@users.noreply.github.com>
2021-09-20 09:59:09 -0600

    remove Inactive from swagger docs

--- application/api/claims.go
+++ application/api/claims.go
@@ -16,7 +16,7 @@ type ClaimIncidentType string
 
 // ClaimStatus
 //
-// may be one of: Draft, Review1, Review2, Review3, Revision, Receipt, Approved, Paid, Denied, Inactive
+// may be one of: Draft, Review1, Review2, Review3, Revision, Receipt, Approved, Paid, Denied
 //
 // swagger:model
 type ClaimStatus string
