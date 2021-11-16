db176b5f50d9866397b6557cb45706b4efceb1c4
Schparky <3172830+Schparky@users.noreply.github.com>
2021-11-16 11:26:39 -0700

    ignore case

--- application/grifts/import.go
+++ application/grifts/import.go
@@ -898,7 +898,7 @@ func importItems(tx *pop.Connection, policyUUID uuid.UUID, policyID int, items [
 			CreatedAt:         time.Time(item.CreatedAt),
 		}
 		for id, name := range names {
-			if name != "" && strings.Contains(newItem.Name, name) {
+			if name != "" && strings.Contains(strings.ToLower(newItem.Name), strings.ToLower(name)) {
 				newItem.PolicyUserID = nulls.NewUUID(id)
 				nNamesMatch++
 				break
