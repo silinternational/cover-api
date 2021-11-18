06f300ad26ef9188ee9ff6fe52f5cb5e973c81e1
Schparky <3172830+Schparky@users.noreply.github.com>
2021-11-18 08:17:50 -0700

    fix bug: item ledger entry date submitted was set to CoverageStartDate

--- application/models/item.go
+++ application/models/item.go
@@ -585,7 +585,6 @@ func (i *Item) Approve(ctx context.Context, doEmitEvent bool) error {
 
 	amount := i.calculateProratedPremium(time.Now().UTC())
 	return i.CreateLedgerEntry(Tx(ctx), LedgerEntryTypeNewCoverage, amount)
-
 }
 
 // Deny takes the item from Pending coverage status to Denied.
@@ -879,6 +878,7 @@ func (i *Item) CreateLedgerEntry(tx *pop.Connection, entryType LedgerEntryType, 
 	i.LoadRiskCategory(tx, false)
 	i.Policy.LoadEntityCode(tx, false)
 
+	now := time.Now().UTC()
 	name := i.GetAccountablePersonName(tx)
 
 	le := NewLedgerEntry(i.Policy, i, nil)
@@ -886,7 +886,7 @@ func (i *Item) CreateLedgerEntry(tx *pop.Connection, entryType LedgerEntryType, 
 	le.Amount = amount
 	le.FirstName = name.First
 	le.LastName = name.Last
-	le.DateSubmitted = i.CoverageStartDate
+	le.DateSubmitted = now
 
 	if err := le.Create(tx); err != nil {
 		return err
@@ -894,7 +894,7 @@ func (i *Item) CreateLedgerEntry(tx *pop.Connection, entryType LedgerEntryType, 
 
 	oldPaidYear := i.PaidThroughYear
 	if le.Type == LedgerEntryTypeNewCoverage {
-		i.PaidThroughYear = time.Now().UTC().Year()
+		i.PaidThroughYear = now.Year()
 	} else if le.Type == LedgerEntryTypeCoverageRefund {
 		i.PaidThroughYear = 0
 	}
