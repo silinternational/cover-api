83e955ba72ae16f5392e7ceafaeb98aa5316a99e
Steve Bagwell <steve_bagwell@sil.org>
2021-12-02 09:18:14 -0500

    move userCreated to new file

---
+++ application/listeners/user.go
@@ -0,0 +1,42 @@
+package listeners
+
+import (
+	"github.com/gobuffalo/events"
+	"github.com/gobuffalo/pop/v5"
+
+	"github.com/silinternational/cover-api/domain"
+	"github.com/silinternational/cover-api/messages"
+	"github.com/silinternational/cover-api/models"
+)
+
+func userCreated(e events.Event) {
+	var user models.User
+	if err := findObject(e.Payload, &user, e.Kind); err != nil {
+		return
+	}
+
+	var householdID string
+	if user.StaffID.Valid {
+		householdID = GetHHID(user.StaffID.String)
+	}
+
+	if err := user.CreateInitialPolicy(nil, householdID); err != nil {
+		domain.ErrLogger.Printf("Failed to create initial policy in %s, %s", e.Kind, err)
+		return
+	}
+
+	// TODO do we need a better way to trigger this
+	userWelcome(e)
+}
+
+func userWelcome(e events.Event) {
+	var user models.User
+	if err := findObject(e.Payload, &user, e.Kind); err != nil {
+		return
+	}
+
+	models.DB.Transaction(func(tx *pop.Connection) error {
+		messages.UserWelcomeQueueMessage(tx, user)
+		return nil
+	})
+}
