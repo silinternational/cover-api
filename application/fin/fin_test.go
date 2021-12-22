18830e697494719bdc394e87a1bbf2623f989457
Schparky <3172830+Schparky@users.noreply.github.com>
2021-12-22 09:16:27 -0700

    fiscal year should be incremented after fiscal start month

--- application/fin/fin_test.go
+++ application/fin/fin_test.go
@@ -2,6 +2,7 @@ package fin
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -30,6 +31,41 @@ func Test_getFiscalPeriod(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.Equal(t, tt.want, getFiscalPeriod(tt.month))
+		})
+	}
+}
+
+func Test_getFiscalYear(t *testing.T) {
+	tests := []struct {
+		name        string
+		fiscalStart int
+		date        time.Time
+		want        int
+	}{
+		{
+			name:        "first day of fiscal year",
+			fiscalStart: 9,
+			date:        time.Date(2021, 9, 1, 0, 0, 0, 0, time.UTC),
+			want:        2022,
+		},
+		{
+			name:        "last day of fiscal year",
+			fiscalStart: 9,
+			date:        time.Date(2021, 8, 31, 0, 0, 0, 0, time.UTC),
+			want:        2021,
+		},
+		{
+			name:        "fiscal start in January",
+			fiscalStart: 1,
+			date:        time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+			want:        2021,
+		},
+	}
+	for _, tt := range tests {
+		domain.Env.FiscalStartMonth = tt.fiscalStart
+
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, getFiscalYear(tt.date))
 		})
 	}
 }
