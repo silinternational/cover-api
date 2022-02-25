527dd6c401fd97e73febd1c47a5949977c23be94
Schparky <3172830+Schparky@users.noreply.github.com>
2022-02-25 09:53:31 -0700

    PR suggestion: combine conditionals to reduce nested if statements

--- application/models/item.go
+++ application/models/item.go
@@ -123,17 +123,15 @@ func (i *Item) Update(ctx context.Context) error {
 		}
 	}
 
-	if oldItem.CoverageAmount != i.CoverageAmount {
-		if i.CoverageStatus == api.ItemCoverageStatusApproved {
-			if oldItem.CoverageAmount < i.CoverageAmount {
-				err := errors.New("item coverage amount cannot be increased")
-				return api.NewAppError(err, api.ErrorItemCoverageAmountCannotIncrease, api.CategoryUser)
-			}
-			if !i.CoverageEndDate.Valid {
-				amount := i.calculatePremiumChange(time.Now().UTC(), oldItem.CoverageAmount)
-				if err := i.CreateLedgerEntry(Tx(ctx), LedgerEntryTypeCoverageChange, amount); err != nil {
-					return err
-				}
+	if i.CoverageStatus == api.ItemCoverageStatusApproved {
+		if oldItem.CoverageAmount < i.CoverageAmount {
+			err := errors.New("item coverage amount cannot be increased")
+			return api.NewAppError(err, api.ErrorItemCoverageAmountCannotIncrease, api.CategoryUser)
+		}
+		if oldItem.CoverageAmount != i.CoverageAmount && !i.CoverageEndDate.Valid {
+			amount := i.calculatePremiumChange(time.Now().UTC(), oldItem.CoverageAmount)
+			if err := i.CreateLedgerEntry(Tx(ctx), LedgerEntryTypeCoverageChange, amount); err != nil {
+				return err
 			}
 		}
 	}
