239c9a72c1e0b380d0557f736e23c9774b1413db
briskt <3172830+briskt@users.noreply.github.com>
2023-08-30 07:29:05 -0600

    PR feedback: use log.Fatalf instead of panic

--- application/models/user.go
+++ application/models/user.go
@@ -202,7 +202,7 @@ func GetDefaultSteward(tx *pop.Connection) User {
 func GetServiceUser(tx *pop.Connection) User {
 	u := User{}
 	if err := tx.Find(&u, ServiceUserID); err != nil {
-		panic("error finding service user" + err.Error())
+		log.Fatalf("error finding service user, %s", err)
 	}
 	return u
 }
