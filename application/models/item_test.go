796ddc9d9562c400e19dc6308b88e5b072beb64e
briskt <3172830+briskt@users.noreply.github.com>
2023-09-19 13:36:07 -0600

    revert partial change to a test

--- application/models/item_test.go
+++ application/models/item_test.go
@@ -561,7 +561,7 @@ func (ms *ModelSuite) TestItem_SafeDeleteOrInactivate() {
 
 	fixConfig := FixturesConfig{
 		NumberOfPolicies: 1,
-		ItemsPerPolicy:   10,
+		ItemsPerPolicy:   9,
 	}
 
 	fixtures := CreateItemFixtures(ms.DB, fixConfig)
