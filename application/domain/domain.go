e67056c218f27181220c57139839c096701b5d33
briskt <3172830+briskt@users.noreply.github.com>
2023-09-28 10:43:36 -0600

    PR review: use the correct Env field name
    Co-authored-by: Jason Jackson <35783387+jason-jackson@users.noreply.github.com>

--- application/domain/domain.go
+++ application/domain/domain.go
@@ -256,7 +256,7 @@ func readEnv() *EnvStruct {
 	env.DependentAutoApproveMax *= CurrencyFactor
 	env.PremiumMinimum *= CurrencyFactor
 	env.RepairThresholdString = fmt.Sprintf("%.2g%%", env.RepairThreshold*100)
-	env.DeductibleString = fmt.Sprintf("%.2g%%", env.Deductible*100)
+	env.DeductibleMinimumString = fmt.Sprintf("%.2g%%", env.Deductible*100)
 
 	//  Set an arbitrary but reasonable minimum lifetime for policy strikes
 	if env.StrikeLifetimeMonths < 2 {
